d0d088c7d2dbc7cc0504ab31ab29eb8fd2658a18
Lakshmi <Lakshmi@DESKTOP-1ABT9ES>
2023-05-05 11:25:34 +0530

    html report has been generated

---
+++ BankingProjectPrac/VTigerProject/Generic/WebDriverUtility.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankingProjectPrac.VTigerProject.Generic
+{
+    internal class WebDriverUtility
+    {
+    }
+}
