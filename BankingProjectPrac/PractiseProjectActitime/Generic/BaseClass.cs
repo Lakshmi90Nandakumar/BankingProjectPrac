20d7d85dd2e89d5d94f1f210cbc9354d4c1586d7
Lakshmi <Lakshmi@DESKTOP-1ABT9ES>
2023-05-05 15:57:07 +0530

    Extend reports and screenshot is added

---
+++ BankingProjectPrac/PractiseProjectActitime/Generic/BaseClass.cs
@@ -0,0 +1,44 @@
+﻿using AventStack.ExtentReports;
+using AventStack.ExtentReports.Reporter;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankingProjectPrac.PractiseProjectActitime.Generic
+{
+    [TestClass]
+    public class BaseClass
+    {
+        public IWebDriver driver;
+        public static ExtentReports extentReports;
+        public string screenShotPath;
+        public ExtentTest extentTest;
+        public static ExtentHtmlReporter htmlReporter;
+        public static string testResultPath = "C:\\Users\\LENOVO\\source\\repos\\BankingProjectPrac\\BankingProjectPrac\\PractiseProjectActitime\\Generic\\Report\\";
+
+        [AssemblyInitialize]
+        public static void AssemblyInitialize(TestContext testContext) 
+        {
+            extentReports = new ExtentReports();
+            htmlReporter = new ExtentHtmlReporter(testResultPath);
+            htmlReporter.Start();
+            extentReports.AttachReporter(htmlReporter);
+            Console.WriteLine("Assembly Initialized");
+
+        
+        }
+        [AssemblyCleanup]
+        public static void AssemblyCleanup() 
+        {
+            Console.WriteLine("Assembly clean up");
+            extentReports.Flush();
+            htmlReporter.Stop();
+            
+        }
+    }
+}
---
+++ BankingProjectPrac/PractiseProjectActitime/Generic/ExtendReportUtitlity.cs
@@ -0,0 +1,61 @@
+﻿using AventStack.ExtentReports;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+
+namespace BankingProjectPrac.PractiseProjectActitime.Generic
+{
+    [TestClass]
+    public class ExtendReportUtitlity:BaseClass
+    {
+        [TestInitialize]
+        public void TestInitialize()
+        {
+                      
+            driver = new ChromeDriver();
+        }
+        [TestMethod]
+        [TestCategory("ExtentHtmlReport")]
+        public void ActiTime()
+        {
+            extentTest = extentReports.CreateTest("ActiTime");
+
+            driver.Url = "http://localhost:8888/";
+
+            ITakesScreenshot takesScreenshot = (ITakesScreenshot)driver;
+            var screenShot=takesScreenshot.GetScreenshot();
+            screenShotPath = "C:\\Users\\LENOVO\\source\\repos\\BankingProjectPrac\\BankingProjectPrac\\PractiseProjectActitime\\Generic\\ScreenShot\\ActiTime.png";
+            screenShot.SaveAsFile(screenShotPath, ScreenshotImageFormat.Png);
+            extentTest.Log(Status.Info, "Taken screenshot for actitime");
+            bool condition = false;
+            if(condition==true)
+            {
+                Assert.IsTrue(true);
+                extentTest.Pass("Test Passed");
+            }
+            else
+            {
+                try
+                {
+                    Assert.IsTrue(false);
+
+                }
+                catch(Exception ex) 
+                {
+                    extentTest.Fail("Test failed");
+                     
+                
+                }
+            }
+            
+        }
+        [TestCleanup]
+        public void Cleanup()
+        {
+            driver.Close();
+            driver.Dispose();
+            extentTest.AddScreenCaptureFromPath(screenShotPath);
+        }
+    }
+}
