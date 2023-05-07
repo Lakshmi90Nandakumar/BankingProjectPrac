c32f02985de564bd0b958d16483115f76dd4318c
Lakshmi <Lakshmi@DESKTOP-1ABT9ES>
2023-05-07 22:18:12 +0530

    webdriverUtility class has been added

---
+++ AutomationProject/Generic/BaseClas.cs
@@ -0,0 +1,28 @@
+﻿using AventStack.ExtentReports;
+using AventStack.ExtentReports.Reporter;
+using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutomationProject.Generic
+{
+    [TestClass]
+    public class BaseClas
+    {
+        public static ExtentReports extentReports;
+        public static ExtentHtmlReporter htmlReporter;
+        public static string htmlReportPath="C:\Users\LENOVO\source\repos\BankingProjectPrac\AutomationProject\Generic\Reports\";
+        public ExtentTest extentTest;
+        public IWebDriver driver;
+
+        [AssemblyInitialize]
+        public static void AssemblyIntitial(TestContext context)
+        {
+
+        }
+
+    }
+}
---
+++ AutomationProject/MSTestScript/LoginPageTest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutomationProject.MSTestScript
+{
+    [TestClass]
+    public class LoginPageTest
+    {
+        [TestMethod]
+        [TestCategory("LoginTestVTiger")]
+        public void LoginTest()
+        {
+
+
+        }
+    }
+}
